f46a2c8405496895e5a7025afa8d558ff54d1314
leticiac.navarro <leticiac.navarro@gmail.com>
2026-04-15 21:59:13 -0300

    no optimize

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-// @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config'; // 1. Adicionamos o passthroughImageService aqui em cima
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	base: '/',
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap()],
+    image: {
+        service: passthroughImageService(), // 2. Essa linha desliga o motor de otimização (Sharp)
+    },
 });
